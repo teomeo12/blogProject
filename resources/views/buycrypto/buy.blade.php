7cd1762aae4ae9122e2d54b4bb9cff09779b6db6
teomeo12 <d00240834@student.dkit.ie>
2022-05-01 23:29:27 +0100

    BuyCrypto page - Logo sizes -fixed

--- resources/views/buycrypto/buy.blade.php
+++ resources/views/buycrypto/buy.blade.php
@@ -9,10 +9,10 @@
             </h2>
         </div>
     </div>
-    <div class="w-5.5/6 m-auto text-center pl-8">
+    <div class="w-4/5 m-auto text-center pl-8">
         <table class="px-72 rounded-md">
             <thead class="bg-gray-50">
-                <tr> 
+                <tr>
                     <th class="px-12 py-2 text-xs text-gray-500 w-40 text-left">Broker</th>
                     <th class="px-6 py-2 text-xs text-gray-500 text-left">Rating</th>
                     <th class="px-6 py-2 text-xs text-gray-500 text-left">Fees</th>
@@ -22,47 +22,50 @@
                 </tr>
             </thead>
             <tbody class="bg-white">
-                <tr class="whitespace-nowrap py-4"> 
+                <tr class="whitespace-nowrap py-4">
                     <td class="py-4">
                         <div class="text-sm text-gray-900">
-                            <img class="h-8 w-40" src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/ftxus.png">
+                            <img class="w-10 h-10 "
+                                src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/ftxus.png">
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">5.0 / 5</p>
-                        <p class="text-gray-400 text-left">Best for Crypto exchanges</p>
+                            <p class="text-left">5.0 / 5</p>
+                            <p class="text-gray-400 text-left">Best for Crypto exchanges</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">0%-0.4%</p>
+                            <p class="text-left">0%-0.4%</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                        $0
+                            $0
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">None</p>
-                        <p class="text-gray-400 text-left">no promotion available at this time</p>
+                            <p class="text-left">None</p>
+                            <p class="text-gray-400 text-left">no promotion available at this time</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                            <a href="https://ftx.com/" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+                            <a href="https://ftx.com/"
+                                class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
                                 Learn More
                             </a>
-                        <p class="text-gray-400 text-left pt-2">on FTX.US's website</p>
+                            <p class="text-gray-400 text-left pt-2">on FTX.US's website</p>
                         </div>
                     </td>
                 </tr>
-                <tr class="whitespace-nowrap py-4"> 
+                <tr class="whitespace-nowrap py-4">
                     <td class="py-4">
                         <div class="text-sm text-gray-900">
-                            <img class="h-8 w-40" src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/Coinbase_Wordmark.png">
+                            <img class="h-8 w-40"
+                                src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/Coinbase_Wordmark.png">
                         </div>
                     </td>
                     <td class="px-6 py-4">
@@ -79,28 +82,30 @@
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                        $2
+                            $2
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">Earn $5</p>
-                        <p class="text-gray-400 text-left">in bitcoin for getting started on coinbase</p>
+                            <p class="text-left">Earn $5</p>
+                            <p class="text-gray-400 text-left">in bitcoin for getting started on coinbase</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                            <a href="https://www.coinbase.com/" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+                            <a href="https://www.coinbase.com/"
+                                class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
                                 Learn More
                             </a>
-                        <p class="text-gray-400 text-left pt-2">on Coinbase's website</p>
+                            <p class="text-gray-400 text-left pt-2">on Coinbase's website</p>
                         </div>
                     </td>
                 </tr>
-                <tr class="whitespace-nowrap py-4"> 
+                <tr class="whitespace-nowrap py-4">
                     <td class="py-4">
                         <div class="text-sm text-gray-900">
-                            <img class="h-8 w-40" src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/gemini.png">
+                            <img class="h-8 w-40"
+                                src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/gemini.png">
                         </div>
                     </td>
                     <td class="px-6 py-4">
@@ -117,28 +122,30 @@
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                        $0
+                            $0
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">$20 of BTC</p>
-                        <p class="text-gray-400 text-left">for new users after trading $100 or more within 30 days</p>
+                            <p class="text-left">$20 of BTC</p>
+                            <p class="text-gray-400 text-left">for new users after trading $100 or more within 30 days</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                            <a href="https://www.gemini.com/eu" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+                            <a href="https://www.gemini.com/eu"
+                                class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
                                 Learn More
                             </a>
-                        <p class="text-gray-400 text-left pt-2">on Gemini's website</p>
+                            <p class="text-gray-400 text-left pt-2">on Gemini's website</p>
                         </div>
                     </td>
                 </tr>
-                <tr class="whitespace-nowrap py-4"> 
+                <tr class="whitespace-nowrap py-4">
                     <td class="py-4">
                         <div class="text-sm text-gray-900">
-                            <img class="h-14 w-40" src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/etoro_logo.png">
+                            <img class="h-14 w-40"
+                                src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/etoro_logo.png">
                         </div>
                     </td>
                     <td class="px-6 py-4">
@@ -155,28 +162,30 @@
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                        $10
+                            $10
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">Get $10</p>
-                        <p class="text-gray-400 text-left">when you buy $100 worth of crypto</p>
+                            <p class="text-left">Get $10</p>
+                            <p class="text-gray-400 text-left">when you buy $100 worth of crypto</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                            <a href="https://www.etoro.com/crypto/exchange/" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+                            <a href="https://www.etoro.com/crypto/exchange/"
+                                class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
                                 Learn More
                             </a>
-                        <p class="text-gray-400 text-left pt-2">on eToro's website</p>
+                            <p class="text-gray-400 text-left pt-2">on eToro's website</p>
                         </div>
                     </td>
                 </tr>
-                <tr class="whitespace-nowrap py-4"> 
+                <tr class="whitespace-nowrap py-4">
                     <td>
                         <div class="text-sm text-gray-900">
-                            <img class="h-8 w-40" src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/Kraken.png">
+                            <img class="h-8 w-40"
+                                src="https://www.nerdwallet.com/cdn-cgi/image/quality=85/cdn/investing/logos/Kraken.png">
                         </div>
                     </td>
                     <td class="px-6 py-4">
@@ -193,27 +202,28 @@
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                        $0
+                            $0
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900">
-                        <p class="text-left">None</p>
-                        <p class="text-gray-400 text-left">no promotion available at this time</p>
+                            <p class="text-left">None</p>
+                            <p class="text-gray-400 text-left">no promotion available at this time</p>
                         </div>
                     </td>
                     <td class="px-6 py-4">
                         <div class="text-sm text-gray-900 text-left">
-                            <a href="https://www.kraken.com/" class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
+                            <a href="https://www.kraken.com/"
+                                class="bg-blue-500 hover:bg-blue-700 text-white text-center py-2 px-4 rounded">
                                 Learn More
                             </a>
-                        <p class="text-gray-400 text-left pt-2">on Kraken's website</p>
+                            <p class="text-gray-400 text-left pt-2">on Kraken's website</p>
                         </div>
                     </td>
                 </tr>
-            </tbody>      
-          </table>
+            </tbody>
+        </table>
     </div>
-        
+
     </div>
 @endsection
